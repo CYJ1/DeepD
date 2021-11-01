e4de3133c5a5bf6c0f897863c5af5542a61989e7
Dahyunne <ryudh98@gmail.com>
2021-11-02 01:20:14 +0900

    Appbar에 뒤로가기 삭제, 홈버튼 추가

--- lib/View/view_result.dart
+++ lib/View/view_result.dart
@@ -3,6 +3,7 @@
 import 'dart:io';
 
 import 'package:deep_d/Controller/controller.dart';
+import 'package:deep_d/View/view_main.dart';
 import 'package:flutter/material.dart';
 import 'package:deep_d/View/view_search.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
@@ -17,60 +18,72 @@ class ResultPage extends StatefulWidget {
 class _ResultPageState extends StateMVC<ResultPage> {
   @override
   var selectedImage = Controller().selectedImage;
+  bool isLoading = true;
+
+  //사진 판별 중엔 isLoading true, 검색 다 되면 isLoading false로 바꿔주어 화면 보이게 하기.
+
   Widget build(BuildContext context) {
     print("결과창입니다");
     print("selectedImage in result: " + selectedImage.path);
     return Scaffold(
         appBar: AppBar(
-          title: Text('YHHY'),
-          centerTitle: true,
-          //홈버튼 넣기
-          //홈버튼 클릭 시 이전꺼 다 사라지게 (뒤로가기 했을때 이전검색 안나오게)
-        ),
-        body: Padding(
-            padding: EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 0.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 300.0,
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Center(
-                                child: Text('검색결과창이 뜹니다 ...'),
-                              ),
-                              Image.file(File(selectedImage.path))
-                            ])),
-                  ),
-                  Divider(
-                    height: 60.0,
-                    color: Colors.grey,
-                    thickness: 1.0,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      print("사진 검색 클릭");
-                      Controller.searchSendImageToServer();
-                      //사진 검색으로 넘기기
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SearchResultPage()));
-                    },
-                    child: Text('사진 검색'),
-                    style: TextButton.styleFrom(
-                      primary: Colors.redAccent,
-                      onSurface: Colors.grey[200],
-                      minimumSize: Size(200, 80),
-                      elevation: 3.0,
-                      backgroundColor: Colors.red[50],
-                      shape: StadiumBorder(),
-                      textStyle: TextStyle(fontSize: 20.0),
-                    ),
-                  ),
-                ])));
+            title: Text('YHHY'),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => View()));
+                },
+                icon: const Icon(Icons.home))
+            //홈버튼 클릭 시 이전꺼 다 사라지게 (뒤로가기 했을때 이전검색 안나오게)
+            ),
+        body: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Padding(
+                padding: EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 0.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 300.0,
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Center(
+                                    child: Text('검색결과창이 뜹니다 ...'),
+                                  ),
+                                  Image.file(File(selectedImage.path))
+                                ])),
+                      ),
+                      Divider(
+                        height: 60.0,
+                        color: Colors.grey,
+                        thickness: 1.0,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          print("사진 검색 클릭");
+                          Controller.searchSendImageToServer();
+                          //사진 검색으로 넘기기
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SearchResultPage()));
+                        },
+                        child: Text('사진 검색'),
+                        style: TextButton.styleFrom(
+                          primary: Colors.redAccent,
+                          onSurface: Colors.grey[200],
+                          minimumSize: Size(200, 80),
+                          elevation: 3.0,
+                          backgroundColor: Colors.red[50],
+                          shape: StadiumBorder(),
+                          textStyle: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                    ])));
   }
 }
--- lib/View/view_search.dart
+++ lib/View/view_search.dart
@@ -23,10 +23,17 @@ class _SearchResultPageState extends StateMVC<SearchResultPage> {
     print("selectedImage in search: " + selectedImage.path);
     return Scaffold(
         appBar: AppBar(
-          title: Text('YHHY'),
-          centerTitle: true,
-          //홈버튼 넣기
-        ),
+            title: Text('YHHY'),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => View()));
+                },
+                icon: const Icon(Icons.home))
+            //홈버튼 클릭 시 이전꺼 다 사라지게 (뒤로가기 했을때 이전검색 안나오게)
+            ),
         body: Padding(
             padding: EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 0.0),
             child: Column(
