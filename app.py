96f8dbfd15cd6887c2d8885d70ee6f7825fce94a
hj3470 <58258321+hj3470@users.noreply.github.com>
2021-11-05 17:34:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask
-from flask import request
-import ssl
+from flask import Flask, request
 
 app = Flask(__name__)
+	    
+@app.route('/')
+def index():
+    return "index"
 
-@app.route('/root')
-def index() :
-    return "root"
+@app.route('/upload')
+def receive_image():
+    data = request.get_json() #data = lib>View>view_main>_getImage>image 값
+    return data
+
+@app.route('/module')
+def module():
+    #모델 불러와서 돌림
+    return 
+
+@app.route('/download')
+def send_result():
+    result = 'true' #lib>View>view_result.dart>printResult로 반환
+    return result
+def send_similarity():
+    similarity = '75%' #lib>View>view_search.dart>printSearchResult로 반환
+    return similarity
 
 if __name__=='__main__':
-    app.run(host='192.168.1.194', port=8080) #host='본인 ip'
+    app.run(host='172.30.1.25', port=8080) #host='본인 ip'
 
