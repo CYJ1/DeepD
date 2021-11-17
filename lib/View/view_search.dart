c68972aea3ece92e9fd22f231b96bc115c95c7fa
hj3470 <58258321+hj3470@users.noreply.github.com>
2021-11-18 05:14:43 +0900

    Update view_search.dart
    server 연결

--- lib/View/view_search.dart
+++ lib/View/view_search.dart
@@ -7,6 +7,43 @@ import 'package:deep_d/View/view_main.dart';
 import 'package:flutter/material.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
 
+//server_start
+import 'package:http/http.dart' as http;
+
+Future<String> getSimilarity() async {
+  http.Response res = await http.get('http://192.168.43.236/downloadsimilarity'); //host ip
+  return res.body
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+Future<Post> fetchPost() async {
+  final response =
+  await http.get('http://192.168.43.236/downloadsimilarity');  //host ip
+
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
+  }
+}
+//server_end
+
 class SearchResultPage extends StatefulWidget {
   const SearchResultPage({Key? key}) : super(key: key);
 
@@ -24,6 +61,13 @@ class _SearchResultPageState extends StateMVC<SearchResultPage> {
   //사진 검색 후 유사도 받아오기.
 
   void printSearchResult() {
+    
+    //server_start
+    Future<String> sim;
+    sim = getSimilarity();
+    similarity = sim
+    //server_end
+      
     searchResult = "사진의 유사도는 " + similarity.toString() + "% 입니다.";
     print(searchResult);
   }
